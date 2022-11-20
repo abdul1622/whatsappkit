3b13dfb29da8a7fda411fe9d90550f0a12adba36
abdul1622 <abdulrahmans@venzotechnologies.com>
2022-11-20 17:40:00 +0530

    main.py

--- main.py
+++ main.py
@@ -1,53 +1,85 @@
 from core import create_app
-import threading
-import asyncio
-from flask import jsonify
-import pandas as pd
+from flask import jsonify,request
 import openpyxl
 from datetime import date,datetime
-from dateutil.parser import parse
+from flask_apscheduler import APScheduler
+import time
+from pywhatkit.whats import sendwhats_image
 
 app = create_app()
 app.debug = True
+scheduler = APScheduler()
+scheduler.init_app(app)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run
 
 def hello():
     print(hello)
 
 @app.route('/index',methods=['GET'])
-async def index():
-    # df = pd.read_excel(r'details.xlsx')
-    df = openpyxl.load_workbook("details.xlsx")
-    df1 = df.active
-    print(df,df.active)
+def index():
+    folder = 'E:\Programming'
+    group = 'Project batch 7'
+    xml_file = (openpyxl.load_workbook("details.xlsx")).active
     today = date.today()
-    # for row in range(0, df1.max_row):
-    for col in df1.iter_cols(1,df1.max_column):
-        for i in col:
-            print(i.value)
-            if i.value == 'dob':
-                date_of_birthlist = col
-        
+    persons = []
+    birth_day_persens = []
+    keys =[]
+    h = list(xml_file.rows)[0]
+    for i in h:
+        keys.append(i.value)
+    print(h,keys)
+    for row in xml_file.iter_rows(2, xml_file.max_row):
+        data = {}
+        for i in range(0,len(row)):
+            data[keys[i]] = row[i].value
+        persons.append(data)
+                
+    print(persons)
+    for i in persons:
+        if isinstance(i['dob'],str):
+            print(i['dob'])
+            i['dob'] = datetime.strptime(i['dob'],'%d/%m/%Y')
+        if i['dob'].day == today.day and i['dob'].month == today.month:
+            birth_day_persens.append(i)
 
-    for row in df1.iter_rows(1, df1.max_row):
-        print(row)
-        for i in row:
-            print(i.value)
-        
+    print(birth_day_persens ,'hi')
+    for i in birth_day_persens:
+        image_path = folder+'/'+ i['image'] +'.png'
+        # sendwhats_image(group,image_path,'hi')
+    return jsonify({'data':'success'})
+# import time
+
+
+def print_date_time():
+    print(time.strftime("%A, %d. %B %Y %I:%M:%S %p"))
+
+# to start sheduler path to xl file and time message
+@app.route('/start',methods=['POST'])
+def start():
+    data = request.json
+    if data['job'] == 'stop'  and scheduler.state:
+        scheduler.shutdown()
+        return data
+    file_path = data['path']
+    hour = data['time'][:2]
+    minute = data['time'][3:5]
+    app.apscheduler.add_job(func=print_date_time, trigger='cron',day_of_week='mon-sun',hour=hour,minute=minute,id='dob message')  
+    if not scheduler.state:
+        scheduler.start()
+    return data
     
-    # print(date_of_birthlist)
-    # for i in date_of_birthlist:
-    #     print(i.coordinate)
-    # print()
 
-    for i in date_of_birthlist:
-        if i.value != 'dob':
-            # datem = datetime.strptime(i.value, "%Y-%m-%d %H:%M:%S")
-            datem = i.value
-            print(datem.day,datem.month)
-            # print((i.value)[:3])
-            # date_of_birth = datetime.strptime(i.value, '%b %d').replace(year=date.today.year)
-            # await (date_of_birth == date.today(),hello())
-            # print('hi')
+@app.route('/state',methods=['GET'])
+def state():
+    print(scheduler.state)
+    return {'data':scheduler.state}
+# scheduler = BackgroundScheduler()
+# scheduler.add_job(func=print_date_time, trigger="interval", seconds=10)
+# app.apscheduler.add_job(func=print_date_time, trigger='interval', seconds=10,id='id')
+
+
+# Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
